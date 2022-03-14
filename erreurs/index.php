953928970836a7908aa204c7a4a493f5c3b44f4a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-03-14 10:26:22 +0100

    exercice

--- erreurs/index.php
+++ erreurs/index.php
@@ -1,6 +1,22 @@
 <?php
 
-// Lever une exception
-// Récupérer une exception
-// Traiter une exception
-// Finalisation
+function divide() {
+    // Est-ce que le premier argument est bien un entier
+    // Est-ce que le second argument est bien un entier
+    // Est-ce que le second argument n'est pas zéro
+    // Retourne la division de deux entiers
+}
+
+echo "Hello, world!";
+
+// Tester du code qui peut potentiellement lever une exception
+try {
+    // Lever une exception
+    echo add("1", 2);
+} catch (Exception $exception) {
+    // Récupération et traitement des erreurs
+    echo $exception->getMessage();
+}
+
+// Cette ligne la ne s'affiche pas
+echo "Bye, world!";
