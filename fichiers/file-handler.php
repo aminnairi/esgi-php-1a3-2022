180b12b5665ccf398e03883928bcaf347b0455bc
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-03-21 20:41:57 +0100

    todo comments

--- fichiers/file-handler.php
+++ fichiers/file-handler.php
@@ -1,27 +1,72 @@
 <?php
 
-function debugArray($array) {
+// Affiche les erreurs lorsqu'il y en a
+ini_set("display_errors", 1);
+
+// Affiche tous les types d'erreurs
+error_reporting(E_ALL);
+
+// Démarre une session
+session_start();
+
+// Permet d'afficher correctement un tableau PHP en HTML
+function debug($array) {
     echo "<pre><code>" . print_r($array, true) . "</code></pre>";
 }
 
-// Envoyer un fichier à un serveur PHP
+// 1Mo
+$maximumFileSizeAccepted = 1000000;
+
+foreach ($_FILES["picture"]["name"] as $index => $fileName) {
+    $size = $_FILES["picture"]["size"][$index];
+    $type = $_FILES["picture"]["type"][$index];
+    $destinationPath = __DIR__ . "/uploads/" . $fileName;
+
+    if (!str_starts_with($type, "image/")) {
+        $_SESSION["error"] = "Le type du fichier $fileName est incorrect (doit être une image).";
+        header("Location: http://localhost/esgi-php-1a3-2022/fichiers");
+        die();
+    }
+
+    if ($size > $maximumFileSizeAccepted) {
+        $_SESSION["error"] = "La taille du fichier $fileName est incorrect (doit être inférieur à 1Mo, reçu $size octets).";
+        header("Location: http://localhost/esgi-php-1a3-2022/fichiers");
+        die();
+    }
+
+    if (file_exists($destinationPath)) {
+        $_SESSION["error"] = "Le fichier $fileName existe déjà.";
+        header("Location: http://localhost/esgi-php-1a3-2022/fichiers");
+        die();
+    }
+}
+
+foreach ($_FILES["picture"]["name"] as $index => $fileName) {
+    $destinationPath = __DIR__ . "/uploads/" . $fileName;
+    move_uploaded_file($_FILES["picture"]["tmp_name"][$index], $destinationPath);
+}
+
+header("Location: http://localhost/esgi-php-1a3-2022/fichiers");
 
 // Récupérer le nom d'un fichier reçu par un client
+// echo $_FILES["picture"]["name"];
 
 // Récupérer le type d'un fichier reçu par un client
+// echo $_FILES["picture"]["type"];
 
 // Récupérer la taille d'un fichier reçu par un client
+// echo $_FILES["picture"]["size"];
 
 // Récupérer le nom temporaire d'un fichier reçu par un client
+// echo $_FILES["picture"]["tmp_name"];
 
 // Vérifier si aucune erreur n'est survenue
+// if ($_FILES["picture"]["error"] !== UPLOAD_ERR_PARTIAL) echo "Une erreur est survenue";
 
 // Déplacer un fichier envoyé par un client dans un dossier local pour sauvegarde permanente
+// move_uploaded_file($_FILES["picture"]["tmp_name"], "/home/app/uploads/new-name.png");
 
 // Envoyer plusieurs fichiers
-
-// A faire
-// Formulaire qui puisse uploader plusieurs fichiers
-// Vérifiez si le fichier n'a pas déjà été uploadé
-// Limiter la taille du fichier à 1Mo
-// Je ne veux que des fichiers images (image/png, image/webp, image/jpg, image/jpe, image/jpeg, image/bmp, image/gif)
+// echo $_FILES["picture"]["name"][0];
+// echo $_FILES["picture"]["name"][1];
+// echo $_FILES["picture"]["name"][2];
--- fichiers/index.php
+++ fichiers/index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
+<?php session_start(); ?>
 <html>
     <body>
-        <form method="POST" action="http://localhost/esgi-php-1a3-2022/fichiers/file-handler.php" enctype="multipart/form-data">
-            <input type="file" name="picture[]">
-            <input type="file" name="picture[]">
-            <button type="submit">Envoyer</button>
-        </form>
+        <?php if (isset($_SESSION["error"])): ?>
+            <h2>Erreur</h2>
+            <p><?php echo $_SESSION["error"]; ?></p>
+            <form>
+                <button type="submit">Réessayer</button>
+            </form>
+            <p><?php unset($_SESSION["error"]); ?></p>
+        <?php else: ?>
+            <form method="POST" action="http://localhost/esgi-php-1a3-2022/fichiers/file-handler.php" enctype="multipart/form-data">
+                <input type="file" name="picture[]">
+                <input type="file" name="picture[]">
+                <button type="submit">Envoyer</button>
+            </form>
+        <?php endif; ?>
     </body>
 </html>
